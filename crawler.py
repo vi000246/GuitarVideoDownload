38bb223e97c8e9d2ffb1bb23bfa9add8d5cc541a
vi000246 <vi000246@gmail.com>
2017-03-11 21:00:54 +0800

    改寫程式結構

--- crawler.py
+++ crawler.py
@@ -5,7 +5,6 @@ import re
 class crawler:
     def __init__(self):
         self.session = requests.Session()
-        self.referer = None # 用來存放header要用的referer
         self.downloadRootPath = u'D:\嗚流吉他' # 下載影片的根目錄
 
     def login(self):
@@ -13,41 +12,32 @@ class crawler:
         return s
 
     # 取得存放vimeo影片的iframe的連結 (有可能有多個iframe 要改成回傳list)
-    def getIframeLinks(self,url):
+    def getVideoLinkList(self,url):
         '''
         :param url: 傳入要抓取頁面的url
-        :return:
+        :return:回傳所有的影片下載連結的list
         '''
-        self.referer = url
         # step1 取得頁面上的vimeo iframe連結
         r = self.session.get(url)
-        m = re.search(r'(?P<match>//player.vimeo.com/video/\d*/?[^\"\"]*)', r.text)
-        return m.group('match')if m is not None else '抓不到iframe連結'
+        iframeList = re.findall(r'(?P<match>//player.vimeo.com/video/\d*/?[^\"\"]*)', r.text)
 
-    def getVideoLink(self,url):
-        '''
-        :param url: 傳入vimeo iframe的url
-        :return:
-        '''
-        # step2 取得vimeo iframe裡的內容
+        # step2 loop list裡的iframe url 取得影片連結
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-            'Referer': self.referer
+            'Referer': url
         }
+        videoLinkList = []
+        for i,url in enumerate(iframeList):
+            r2 = self.session.get('https:' + url, headers=headers)
 
-        r2 = self.session.get('https:' + url, headers=headers)
+            # 找出vimeo 影片的下載連結
+            m2 = re.findall(r'(?P<match>https?://[0-9a-zA-Z-]*.vimeocdn.com/[a-z-\d/]+.mp4[^\"]*)', r2.text)
 
-        # 找出vimeo 影片的下載連結
-        m2 = re.search(r'(?P<match>https?://[0-9a-zA-Z-]*.vimeocdn.com/[a-z-\d/]+.mp4[^\"]*)', r2.text)
+            videoLinkList = videoLinkList + m2
 
-        return m2.group('match') if m2 is not None else '抓不到vimeo mp4檔'
+        return videoLinkList
 
     def downloadVideo(self,url,path):
-        '''
-        :param url: 影片下載連結
-        :param path: 根目錄後的子目錄跟檔名  例如: 大分類\小分類\xxxx.mp4
-        :return:
-        '''
         r = self.session.get(url, stream=True)
         print(u'開始下載影片'+path)
         with open(self.downloadRootPath+'\\'+ path, 'wb') as f:
@@ -59,7 +49,7 @@ class crawler:
 
 if __name__ =="__main__":
     cw = crawler()
-    iflink = cw.getIframeLinks('http://realsound.tw/members/style-step2/')
-    videolink = cw.getVideoLink(iflink)
-    cw.downloadVideo(videolink,'test.mp4')
-    print(videolink)
+    videoList = cw.getVideoLinkList('http://realsound.tw/members/style-step2/')
+    for i,url in enumerate(videoList):
+        cw.downloadVideo(url,'test.mp4')
+
