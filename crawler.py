88dfb3d2f39c82a0d73cd9a4f9652f33daf91068
vi000246 <vi000246@gmail.com>
2017-03-12 21:47:36 +0800

    加入progress bar

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import requests
 import re
+import sys
 
 class crawler:
     def __init__(self):
@@ -20,7 +21,8 @@ class crawler:
         # step1 取得頁面上的vimeo iframe連結
         r = self.session.get(url)
         iframeList = re.findall(r'(?P<match>//player.vimeo.com/video/\d*/?[^\"\"]*)', r.text)
-
+        if iframeList is None:
+            raise '找不到vimeo的iframe'
         # step2 loop list裡的iframe url 取得影片連結
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
@@ -32,19 +34,31 @@ class crawler:
 
             # 找出vimeo 影片的下載連結
             m2 = re.findall(r'(?P<match>https?://[0-9a-zA-Z-]*.vimeocdn.com/[a-z-\d/]+.mp4[^\"]*)', r2.text)
-
+            if m2 is None:
+                raise '找不到vimeo影片的下載連結'
             videoLinkList = videoLinkList + m2
 
         return videoLinkList
 
     def downloadVideo(self,url,path):
         r = self.session.get(url, stream=True)
+        total_length = r.headers.get('content-length')
+
         print(u'開始下載影片'+path)
         with open(self.downloadRootPath+'\\'+ path, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:  # filter out keep-alive new chunks
-                    f.write(chunk)
-                    # f.flush() commented by recommendation from J.F.Sebastian
+            if total_length is None:  # no content length header
+                f.write(r.content)
+            else:
+                dl = 0
+                total_length = int(total_length)
+                for chunk in r.iter_content(chunk_size=1024):
+                    dl += len(chunk)
+                    if chunk:  # filter out keep-alive new chunks
+                        f.write(chunk)
+                        done = int(50 * dl / total_length)
+                        # progress bar
+                        sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                        sys.stdout.flush()
         return path
 
 if __name__ =="__main__":
